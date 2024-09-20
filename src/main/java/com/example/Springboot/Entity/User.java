d89692cfbdf891bd560f71bc969ed79032394d2f
Ahmed4Magdy <ahmedmagdi14402@gmail.com>
2024-09-20 20:25:41 +0300

    signup

---
+++ src/main/java/com/example/Springboot/Entity/User.java
@@ -0,0 +1,49 @@
+package com.example.Springboot.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String fullname;
+    private String email;
+    private String password;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
